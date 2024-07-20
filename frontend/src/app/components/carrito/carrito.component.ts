c0973811e2570032bf1a16b18cb5900896950efe
BryanMoreno11 <114695758+BryanMoreno11@users.noreply.github.com>
2024-07-20 18:06:51 -0500

    frontend del carrito terminado

--- frontend/src/app/components/carrito/carrito.component.ts
+++ frontend/src/app/components/carrito/carrito.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CarritoService, Carrito, Producto } from '../../services/carrito.service';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-carrito',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './carrito.component.html',
   styleUrl: './carrito.component.css'
 })
@@ -34,6 +36,63 @@ export class CarritoComponent {
       this.carrito_service.setCantidad(producto, cantidad);
     }
   }
+
+  aumentar(producto:any){
+    producto.cantidad++;
+    this.actualizarCantidad(producto, producto.cantidad);
+  }
+
+  decrementar(producto:any){
+    if(producto.cantidad>1){
+      producto.cantidad--;
+    this.actualizarCantidad(producto, producto.cantidad);
+    }
+  }
+
+  eliminarProducto(producto:any){
+    Swal.fire({
+      title: "¿Estás seguro de eliminar este producto del carrito?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#d33",
+      cancelButtonColor: "#3085d6",
+      confirmButtonText: "Sí, eliminar!",
+      cancelButtonText: "Cancelar"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.carrito_service.eliminarProducto(producto);
+        this.productos=this.carrito_service.productos;
+        Swal.fire({
+          title: "¡Producto Eliminado!",
+          icon: "success"
+        });
+      }
+    });
+  }
+
+  realizarCompra(){
+      Swal.fire({
+        title: "¿Estás seguro de proceder con la compra?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sí, continuar",
+        cancelButtonText: "Cancelar"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.carrito_service.efectuarCompra();
+          this.productos=this.carrito_service.productos;
+          this.carrito=this.carrito_service.carrito;
+          Swal.fire({
+            title: "¡Compra efectuada con éxito!",
+            icon: "success"
+          });
+        }
+      });
+   }
+
+
 
 
 }
