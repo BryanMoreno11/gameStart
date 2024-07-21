05eabb52af2d3df80d21b6f02eb980b760555311
BryanMoreno11 <114695758+BryanMoreno11@users.noreply.github.com>
2024-07-21 08:32:01 -0500

    servicio venta primera version

---
+++ frontend/src/app/services/venta.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VentaService {
+
+  API_URL='http://localhost:3000/api/';
+  //#region Métodos
+  constructor(private http:HttpClient) {
+  }
+  
+  insertVenta(venta:Venta){
+    return this.http.post(`${this.API_URL}venta`,venta);
+  }
+
+
+}
+
+export interface Venta{
+  id_cliente:number | string;
+  total_venta:number;
+  subtotal:number;
+  iva:number;
+}
+
+export interface VentaDetalle{
+  id_venta:number | string;
+  id_videojuego_plataforma:number | string;
+  cantidad_vendida:number;
+  subtotal:number;
+  iva:number;
+}
